a20ad7d1428caafb7bf167c7cdfd01f484ca3f72
Justin Crosby <justin.crosby14@gmail.com>
2026-01-16 18:08:47 -0800

    Remove html5 notify, update pyscript scripts

--- pyscript/media_scripts.py
+++ pyscript/media_scripts.py
@@ -11,52 +11,52 @@ description: Sets all audio devices to an optimal volume
     bathroom_vol = 0.5
     # Try to set the volume 10 times and if we fail exit
     max_retries = 10
-    if(state.get("media_player.home_audio") == "playing"):
+    if state.get("media_player.home_audio") == "playing":
         retry_count = 0
-        while(round(state.getattr("media_player.denon_avr_x1300w")["volume_level"], 2) != receiver_vol):
+        while round(state.getattr("media_player.denon_avr_x1300w")["volume_level"], 2) != receiver_vol:
             media_player.volume_set(entity_id="media_player.denon_avr_x1300w", volume_level=receiver_vol)
             task.sleep(0.5)
-            if(retry_count > max_retries):
+            if retry_count > max_retries:
                 log.warning("Failed to set volume for receiver")
                 return
         vol = round(state.getattr("media_player.denon_avr_x1300w")["volume_level"], 2)
         log.info(f"Set receiver volume to {vol}")
-    if(state.get("media_player.living_room_audio") == "playing"):
+    if state.get("media_player.living_room_audio") == "playing":
         retry_count = 0
-        while(round(state.getattr("media_player.living_room_audio")["volume_level"], 2) != living_room_vol):
+        while round(state.getattr("media_player.living_room_audio")["volume_level"], 2) != living_room_vol:
             media_player.volume_set(entity_id="media_player.living_room_audio", volume_level=living_room_vol)
             task.sleep(0.5)
-            if(retry_count > max_retries):
+            if retry_count > max_retries:
                 log.warning("Failed to set volume for living room audio")
                 return
         vol = round(state.getattr("media_player.living_room_audio")["volume_level"], 2)
         log.info(f"Set living room volume to {vol}")
-    if(state.get("media_player.master_bedroom_audio") == "playing"):
+    if state.get("media_player.master_bedroom_audio") == "playing":
         retry_count = 0
-        while(round(state.getattr("media_player.master_bedroom_audio")["volume_level"], 2) != master_bed_vol):
+        while round(state.getattr("media_player.master_bedroom_audio")["volume_level"], 2) != master_bed_vol:
             media_player.volume_set(entity_id="media_player.master_bedroom_audio", volume_level=master_bed_vol)
             task.sleep(0.5)
-            if(retry_count > max_retries):
+            if retry_count > max_retries:
                 log.warning("Failed to set volume for master bedroom audio")
                 return
         vol = round(state.getattr("media_player.master_bedroom_audio")["volume_level"], 2)
         log.info(f"Set master bedroom volume to {vol}")
-    if(state.get("media_player.master_bathroom_speaker") == "playing"):
+    if state.get("media_player.master_bathroom_speaker") == "playing":
         retry_count = 0
-        while(round(state.getattr("media_player.master_bathroom_speaker")["volume_level"], 2) != bathroom_vol):
+        while round(state.getattr("media_player.master_bathroom_speaker")["volume_level"], 2) != bathroom_vol:
             media_player.volume_set(entity_id="media_player.master_bathroom_speaker", volume_level=bathroom_vol)
             task.sleep(0.5)
-            if(retry_count > max_retries):
+            if retry_count > max_retries:
                 log.warning("Failed to set volume for master bathroom speaker")
                 return
         vol = round(state.getattr("media_player.master_bathroom_speaker")["volume_level"], 2)
         log.info(f"Set master bathroom volume to {vol}")
-    if(state.get("media_player.bathroom_speaker") == "playing"):
+    if state.get("media_player.bathroom_speaker") == "playing":
         retry_count = 0
-        while(round(state.getattr("media_player.bathroom_speaker")["volume_level"], 2) != bathroom_vol):
+        while round(state.getattr("media_player.bathroom_speaker")["volume_level"], 2) != bathroom_vol:
             media_player.volume_set(entity_id="media_player.bathroom_speaker", volume_level=bathroom_vol)
             task.sleep(0.5)
-            if(retry_count > max_retries):
+            if retry_count > max_retries:
                 log.warning("Failed to set volume for bathroom speaker")
                 return
         vol = round(state.getattr("media_player.bathroom_speaker")["volume_level"], 2)
@@ -84,28 +84,28 @@ fields:
     log.info(f"volume fade: step size={step_size}, start volume={start_vol}, end volume={end_vol}")
     entity_id = state.get("sensor.media_player_selected_output")
 
-    if (start_vol == end_vol):
-        if (start_vol == end_vol):
+    if start_vol == end_vol:
+        if start_vol == end_vol:
             log.warning("Passing an end_vol value equal to the start_vol")
             return
 
-    if (start_vol > end_vol):
-        if (step_size > 0):
+    if start_vol > end_vol:
+        if step_size > 0:
             step_size = step_size * -1
     else:
-        if (step_size < 0):
+        if step_size < 0:
             loh.warning("Passing a negative value when end_vol is greater than start_vol")
             return
 
-    while (start_vol != end_vol):
+    while start_vol != end_vol:
         start_vol = start_vol + step_size
-        if (step_size > 0):
+        if step_size > 0:
             # We are increasing the volume, make sure we don't overshoot.
-            if (start_vol >= end_vol):
+            if start_vol >= end_vol:
                 start_vol = end_vol
-        elif (step_size < 0):
+        elif step_size < 0:
             # We are decreasing the volume, make sure we don't overshoot.
-            if (start_vol <= end_vol):
+            if start_vol <= end_vol:
                 start_vol = end_vol
         media_player.volume_set(entity_id=entity_id, volume_level=(float(start_vol) / 100))
         task.sleep(0.5)
@@ -122,43 +122,43 @@ description: Starts the music quietly in the morning
     playing = False
     curr_time = 0
     retries = 2
-    while(not playing):
+    while not playing:
         script.turn_on(entity_id="script.play_chill_music_var", variables={"volume_level":"0"})
         # Timeout after 10s
-        while(curr_time < 10):
+        while curr_time < 10:
             task.sleep(1)
-            if(state.get("sensor.media_player_status") == "playing"):
+            if state.get("sensor.media_player_status") == "playing":
                 playing = True
                 break
             curr_time += 1
         curr_time = 0
         retries += 1
-        if(retries > 2):
+        if retries > 2:
             break
     # Start music at 0 volume and slowly fade in
-    volume_fade(step_size=1, start_vol=0, end_vol=35)
+    pyscript.volume_fade(step_size=1, start_vol=0, end_vol=35)
     # Let the music play for 15 mins
     task.sleep(60*15)
     # Fade back down to 0
-    volume_fade(step_size=5, start_vol=35, end_vol=0)
+    pyscript.volume_fade(step_size=5, start_vol=35, end_vol=0)
     media_player.media_stop(entity_id="media_player.master_bedroom_audio")
     # Switch to whole suite
     input_select.select_option(entity_id="input_select.media_output_select", option="Master Suite Audio")
     # Start up playlist
     playing = False
     curr_time = 0
-    while(not playing):
+    while not playing:
         script.turn_on(entity_id="script.play_all_music_var", variables={"volume_level":"0"})
         # Timeout after 10s
-        while(curr_time < 10):
+        while curr_time < 10:
             task.sleep(1)
-            if(state.get("sensor.media_player_status") == "playing"):
+            if state.get("sensor.media_player_status") == "playing":
                 playing = True
                 break
             curr_time += 1
         curr_time = 0
         retries += 1
-        if(retries > 2):
+        if retries > 2:
             break
     # Start music at 0 volume and slowly fade in
-    volume_fade(step_size=5, start_vol=0, end_vol=45)
+    pyscript.volume_fade(step_size=5, start_vol=0, end_vol=45)
--- pyscript/morning_bedtime_scripts.py
+++ pyscript/morning_bedtime_scripts.py
@@ -4,11 +4,11 @@ def shutdown():
 name: Shutdown
 description: Turn everything off
 """
-    if(state.get("light.bedroom_lights") == "on"):
+    if state.get("light.bedroom_lights") == "on":
         light.turn_on(entity_id="light.bedroom_lights", brightness=0, transition=15)
     light.turn_off(entity_id="light.all_lights")
     homeassistant.turn_off(entity_id="group.media_players")
-    if(state.get("sensor.media_player_status") == "on"):
+    if state.get("sensor.media_player_status") == "on":
         media_player.stop(entity_id=state.get("sensor.media_player_output"))
 
 @service
@@ -20,13 +20,13 @@ description: Gradually wake up in the morning :)
     input_boolean.turn_off(entity_id="input_boolean.bedtime")
     input_boolean.turn_on(entity_id="input_boolean.enable_presence_automations")
     # Only execute the full alarm routine if requested
-    if(state.get("binary_sensor.alarm_full") == "on"):
+    if state.get("binary_sensor.alarm_full") == "on":
         light.turn_on(entity_id="light.nightstand_lights", rgb_color=[255, 0, 0], brightness_pct=1)
         task.sleep(1)
         light.turn_on(entity_id="light.nightstand_lights", rgb_color=[255, 255, 0], brightness_pct=80, transition=300)
         task.sleep(25*60)
         light.turn_on(entity_id="light.nightstand_lights", rgb_color=[255, 255, 255], brightness_pct=100, transition=100)
-        sleep(100)
+        task.sleep(100)
         light.turn_on(entity_id="light.bedroom_lights", rgb_color=[255, 255, 255], brightness_pct=100)
         pyscript.morning_music()
 
@@ -36,7 +36,7 @@ def bedtime():
 name: Bedtime
 description: Good night :)
 """
-    shutdown()
+    pyscript.shutdown()
     input_boolean.turn_on(entity_id="input_boolean.bedtime")
     input_boolean.turn_off(entity_id="input_boolean.enable_presence_automations")
     automation.turn_off(entity_id="automation.bedroom_motion_detected")
