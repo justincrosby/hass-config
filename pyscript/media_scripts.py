57d52bb247ad93001a38ea4dc770d41ced8146e9
Justin Crosby <justin.crosby14@gmail.com>
2026-01-19 15:14:12 -0800

    Update to use grad_vol

--- pyscript/media_scripts.py
+++ pyscript/media_scripts.py
@@ -1,56 +1,74 @@
+# Convert from music assistant media player to google cast media player
+MEDIA_PLAYER_MASS_TRANSLATION = {
+    "media_player.mass_home": "media_player.home_audio",
+    "media_player.mass_fake_home": "media_player.fake_home_audio",
+    "media_player.mass_master_suite": "media_player.master_suite_audio",
+    "media_player.mass_backyard": "media_player.backyard_speaker",
+    "media_player.denon_avr_x1300w": "media_player.denon_avr_x1300w",
+    "media_player.mass_kitchen": "media_player.kitchen_speaker",
+    "media_player.mass_living_room": "media_player.living_room_audio",
+    "media_player.mass_master_bathroom": "media_player.master_bathroom_speaker",
+    "media_player.mass_master_bedroom": "media_player.master_bedroom_audio"
+}
+
+# Default volume levels for media players
+MEDIA_PLAYER_DEFAULT_VOLUME = {
+    "media_player.backyard_speaker": 0.8,
+    "media_player.denon_avr_x1300w": 0.48,
+    "media_player.kitchen_speaker": 0.8,
+    "media_player.living_room_audio": 0.8,
+    "media_player.master_bathroom_speaker": 0.5,
+    "media_player.master_bedroom_audio": 0.8,
+}
+
+# Media player groups definition
+MEDIA_PLAYER_GROUPS = {
+    "media_player.mass_home": [
+        "media_player.backyard_speaker",
+        "media_player.denon_avr_x1300w",
+        "media_player.kitchen_speaker",
+        "media_player.living_room_audio",
+        "media_player.master_bathroom_speaker",
+        "media_player.master_bedroom_audio"
+    ],
+    "media_player.mass_fake_home": [
+        "media_player.backyard_speaker",
+        "media_player.kitchen_speaker",
+        "media_player.living_room_audio",
+        "media_player.master_bathroom_speaker",
+        "media_player.master_bedroom_audio"
+    ],
+
+    "master_suite": [
+        "media_player.master_bathroom_speaker",
+        "media_player.master_bedroom_audio"
+    ]
+}
+
 @service
-def set_default_audio_levels():
-    """yaml
-name: Set Default Audio Levels
-description: Sets all audio devices to an optimal volume
-"""
-    # Defaults here
-    receiver_vol = 0.48
-    living_room_vol = 0.8
-    master_bed_vol = 0.8
-    bathroom_vol = 0.5
-    # Try to set the volume 10 times and if we fail exit
-    max_retries = 10
-    if state.get("media_player.home_audio") == "playing":
-        retry_count = 0
-        while round(state.getattr("media_player.denon_avr_x1300w")["volume_level"], 2) != receiver_vol:
-            media_player.volume_set(entity_id="media_player.denon_avr_x1300w", volume_level=receiver_vol)
-            task.sleep(0.5)
-            if retry_count > max_retries:
-                log.warning("Failed to set volume for receiver")
-                return
-        vol = round(state.getattr("media_player.denon_avr_x1300w")["volume_level"], 2)
-        log.info(f"Set receiver volume to {vol}")
-    if state.get("media_player.living_room_audio") == "playing":
-        retry_count = 0
-        while round(state.getattr("media_player.living_room_audio")["volume_level"], 2) != living_room_vol:
-            media_player.volume_set(entity_id="media_player.living_room_audio", volume_level=living_room_vol)
-            task.sleep(0.5)
-            if retry_count > max_retries:
-                log.warning("Failed to set volume for living room audio")
-                return
-        vol = round(state.getattr("media_player.living_room_audio")["volume_level"], 2)
-        log.info(f"Set living room volume to {vol}")
-    if state.get("media_player.master_bedroom_audio") == "playing":
-        retry_count = 0
-        while round(state.getattr("media_player.master_bedroom_audio")["volume_level"], 2) != master_bed_vol:
-            media_player.volume_set(entity_id="media_player.master_bedroom_audio", volume_level=master_bed_vol)
-            task.sleep(0.5)
-            if retry_count > max_retries:
-                log.warning("Failed to set volume for master bedroom audio")
-                return
-        vol = round(state.getattr("media_player.master_bedroom_audio")["volume_level"], 2)
-        log.info(f"Set master bedroom volume to {vol}")
-    if state.get("media_player.master_bathroom_speaker") == "playing":
-        retry_count = 0
-        while round(state.getattr("media_player.master_bathroom_speaker")["volume_level"], 2) != bathroom_vol:
-            media_player.volume_set(entity_id="media_player.master_bathroom_speaker", volume_level=bathroom_vol)
-            task.sleep(0.5)
-            if retry_count > max_retries:
-                log.warning("Failed to set volume for master bathroom speaker")
-                return
-        vol = round(state.getattr("media_player.master_bathroom_speaker")["volume_level"], 2)
-        log.info(f"Set master bathroom volume to {vol}")
+def set_default_audio_levels(group_name=""):
+    """
+    Set all media players in the specified group to their default volume levels.
+    
+    Args:
+        group_name: The name of the group (key in MEDIA_PLAYER_DEFAULT_VOLUME)
+    """
+    if group_name in MEDIA_PLAYER_GROUPS:
+        entities = MEDIA_PLAYER_GROUPS[group_name]
+    elif group_name in MEDIA_PLAYER_MASS_TRANSLATION:
+        entities = [MEDIA_PLAYER_MASS_TRANSLATION[group_name]]
+    else:
+        log.error(f"Group '{group_name}' not found.")
+        return
+    
+    for entity_id in entities:
+        default_volume = MEDIA_PLAYER_DEFAULT_VOLUME.get(entity_id)
+        task.wait_until(state_trigger="{entity_id} == 'playing'".format(entity_id=entity_id), timeout=5)
+        if default_volume is not None:
+            grad_vol.set_volume(data={"volume": default_volume, "duration": 10}, target={"entity_id": entity_id})
+            log.info(f"Set {entity_id} to default volume level {default_volume}")
+        else:
+            log.warning(f"No default volume level defined for {entity_id}")
 
 @service
 def switch_to_shield():
@@ -65,75 +83,7 @@ description: Turns on the receiver, resets the volume to a known level, and swit
     media_player.select_source(entity_id="media_player.denon_avr_x1300w", source="Shield")
 
 @service
-def volume_fade(step_size=0, start_vol=0, end_vol=0):
-    """yaml
-name: Volume Fade
-description: Slowly fades the volume of the chosen output device from start volume to end volume
-fields:
-  step_size:
-    description: the volume step size in percentage
-    example: 1
-    required: true
-  start_vol:
-    description: the starting volume in percentage
-    example: 0
-    required: true
-  end_vol:
-    description: the ending volume in percentage
-    example: 50
-    required: true
-"""
-    log.info(f"volume fade: step size={step_size}, start volume={start_vol}, end volume={end_vol}")
-    entity_id = state.get("sensor.media_player_selected_output")
-
-    if start_vol == end_vol:
-        if start_vol == end_vol:
-            log.warning("Passing an end_vol value equal to the start_vol")
-            return
-
-    if start_vol > end_vol:
-        if step_size > 0:
-            step_size = step_size * -1
-    else:
-        if step_size < 0:
-            loh.warning("Passing a negative value when end_vol is greater than start_vol")
-            return
-
-    while start_vol != end_vol:
-        start_vol = start_vol + step_size
-        if step_size > 0:
-            # We are increasing the volume, make sure we don't overshoot.
-            if start_vol >= end_vol:
-                start_vol = end_vol
-        elif step_size < 0:
-            # We are decreasing the volume, make sure we don't overshoot.
-            if start_vol <= end_vol:
-                start_vol = end_vol
-        media_player.volume_set(entity_id=entity_id, volume_level=(float(start_vol) / 100))
-        task.sleep(0.5)
-
-@service
-def morning_music():
-    """yaml
-name: Morning Music
-description: Starts the music quietly in the morning
-"""
-    # Start music with bedroom audio
-    input_select.select_option(entity_id="input_select.media_output_select", option="Master Bedroom Audio")
-    # Start up playlist
-    script.turn_on(entity_id="script.play_chill_music_var", variables={"volume_level":"0.35", "volume_step":1})
-    # Let the music play for 15 mins
-    task.sleep(60*15)
-    # Fade back down to 0
-    pyscript.volume_fade(step_size=5, start_vol=35, end_vol=0)
-    media_player.media_stop(entity_id="media_player.mass_master_bedroom")
-    # Switch to whole suite
-    input_select.select_option(entity_id="input_select.media_output_select", option="Master Suite Audio")
-    # Start up playlist
-    script.turn_on(entity_id="script.play_all_music_var", variables={"volume_level":"0.45", "volume_step":5})
-
-@service
-def start_playlist(entity_id="", playlist_uri="", volume_level=None, volume_step=5):
+def start_playlist(entity_id="", playlist_uri="", volume_level=None, duration=20):
     """yaml
 name: Start Playlist
 description: Starts a playlist on the specified device at the specified volume level
@@ -149,18 +99,19 @@ fields:
   volume_level:
     description: the volume level to set (0.0 to 1.0)
     example: 0.5
-    required: false
-  volume_step:
-    description: the volume step size for fading in
-    example: 5
+  duration:
+    description: duration in seconds for volume fade-in/out
+    example: 60
     required: false
 """
     if volume_level is not None:
+        log.info(f"Set volume_level {0.01} on {entity_id}")
         media_player.volume_set(entity_id=entity_id, volume_level=0.01)
     media_player.shuffle_set(entity_id=entity_id, shuffle=True)
     music_assistant.play_media(media_id=playlist_uri, enqueue='replace', entity_id=entity_id)
     task.wait_until(state_trigger="{entity_id} == 'playing'".format(entity_id=entity_id), state_hold=2, timeout=15)
     if volume_level is not None:
-        pyscript.volume_fade(step_size=volume_step, start_vol=0, end_vol=int(volume_level * 100))
-        task.sleep(5)
-        pyscript.set_default_audio_levels()
+        script.turn_on(entity_id="script.gradually_set_volume", variables={"volume": volume_level, "duration": duration, "entity_id": entity_id})
+        log.info(f"Set volume_level {volume_level} on {entity_id} over {duration} seconds")
+        task.sleep(duration)
+        set_default_audio_levels(group_name=entity_id)
